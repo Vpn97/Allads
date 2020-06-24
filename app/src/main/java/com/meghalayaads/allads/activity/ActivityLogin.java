0ad9b4502623f8d854e3047f380a7e4a82d5b17c
Vpn97 <vpn0097@gmail.com>
2020-06-24 09:58:39 +0530

    - update network_security_config - add font - update color config

---
+++ app/src/main/java/com/meghalayaads/allads/activity/ActivityLogin.java
@@ -0,0 +1,16 @@
+package com.meghalayaads.allads.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.meghalayaads.allads.R;
+
+public class ActivityLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
